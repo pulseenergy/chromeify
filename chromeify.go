60dd3512fbf4c404fe5d8852ef01b50c0211451e
Neil Gentleman <ngentleman@gmail.com>
2014-10-17 14:56:08 -0700

    a bit of refactoring

--- chromeify.go
+++ chromeify.go
@@ -10,6 +10,91 @@ import (
 	"image/png"
 )
 
+type Theme struct {
+	topLeft image.Image
+	top image.Image
+	topRight image.Image
+	left image.Image
+	right image.Image
+	bottomLeft image.Image
+	bottom image.Image
+	bottomRight image.Image
+}
+
+func defaultTheme() (theme Theme, err error) {
+	topLeft, err := loadImage("data/top_left.png")
+	if err != nil {
+		return
+	}
+	topRight, err := loadImage("data/top_right.png")
+	if err != nil {
+		return
+	}
+	top, err := loadImage("data/top_center.png")
+	if err != nil {
+		return
+	}
+	border, err := loadImage("data/1x1_border.png")
+	if err != nil {
+		return
+	}
+	return Theme{topLeft, top, topRight, border, border, border, border, border}, nil
+}
+
+func (theme Theme) Decorate(in image.Image) image.Image {
+	outerWidth := theme.left.Bounds().Dx() + in.Bounds().Dx() + theme.right.Bounds().Dx()
+	outerHeight := theme.top.Bounds().Dy() + in.Bounds().Dy() + theme.bottom.Bounds().Dy()
+
+	img := image.NewRGBA(image.Rect(0, 0, outerWidth, outerHeight))
+
+	// pink fill shows any gaps
+	//	pink := color.RGBA{255, 0, 255, 255}
+	//	draw.Draw(img, img.Bounds(), &image.Uniform{pink}, image.ZP, draw.Src)
+	draw.Draw(img, in.Bounds().Add(image.Pt(theme.left.Bounds().Dx(), theme.top.Bounds().Dy())), in, image.ZP, draw.Src)
+
+	// top-left
+	offset := image.ZP
+	draw.Draw(img, theme.topLeft.Bounds().Add(offset), theme.topLeft, image.ZP, draw.Src)
+
+	// top
+	for offset := theme.topLeft.Bounds().Dx(); offset < outerWidth - theme.topRight.Bounds().Dx(); offset += theme.top.Bounds().Dx() {
+		r := theme.top.Bounds().Add(image.Pt(offset, 0))
+		draw.Draw(img, r, theme.top, image.ZP, draw.Src)
+	}
+
+	// top-right
+	offset = image.Pt(outerWidth - theme.topRight.Bounds().Dx(), 0)
+	draw.Draw(img, theme.topRight.Bounds().Add(offset), theme.topRight, image.ZP, draw.Src)
+
+	// left
+	for offset := theme.topLeft.Bounds().Dy(); offset < outerHeight - theme.bottomLeft.Bounds().Dy(); offset += theme.left.Bounds().Dy() {
+		r := theme.left.Bounds().Add(image.Pt(0, offset))
+		draw.Draw(img, r, theme.left, image.ZP, draw.Src)
+	}
+
+	// right
+	for offset := theme.topRight.Bounds().Dy(); offset < outerHeight - theme.bottomRight.Bounds().Dy(); offset += theme.right.Bounds().Dy() {
+		r := theme.right.Bounds().Add(image.Pt(outerWidth - theme.right.Bounds().Dx(), offset))
+		draw.Draw(img, r, theme.right, image.ZP, draw.Src)
+	}
+
+	// bottom-left
+	offset = image.Pt(0, outerHeight - theme.bottomLeft.Bounds().Dy())
+	draw.Draw(img, theme.bottomLeft.Bounds().Add(offset), theme.bottomLeft, image.ZP, draw.Src)
+
+	// bottom
+	for offset := theme.bottomLeft.Bounds().Dx(); offset < outerWidth - theme.bottomRight.Bounds().Dx(); offset += theme.bottom.Bounds().Dx() {
+		r := theme.bottom.Bounds().Add(image.Pt(offset, outerHeight - theme.bottom.Bounds().Dy()))
+		draw.Draw(img, r, theme.bottom, image.ZP, draw.Src)
+	}
+
+	// bottom-right
+	offset = image.Pt(outerWidth - theme.bottomRight.Bounds().Dx(), outerHeight - theme.bottomRight.Bounds().Dy())
+	draw.Draw(img, theme.bottomRight.Bounds().Add(offset), theme.topRight, image.ZP, draw.Src)
+
+	return img
+}
+
 func loadImage(filename string) (image.Image, error) {
 	f, err := os.Open(filename)
 	if err != nil {
@@ -30,83 +115,17 @@ func writeImage(filename string, img image.Image) error {
 }
 
 func main() {
-	topLeft, err := loadImage("data/top_left.png")
+	theme, err := defaultTheme()
 	if err != nil {
 		log.Fatal(err)
 	}
-	topRight, err := loadImage("data/top_right.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-	top, err := loadImage("data/top_center.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-	left, err := loadImage("data/1x1_border.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-	right := left
-	bottom := left
-	bottomLeft := left
-	bottomRight := left
 
 	page, err := loadImage("data/sample_page.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	outerWidth := left.Bounds().Dx() + page.Bounds().Dx() + right.Bounds().Dx()
-	outerHeight := top.Bounds().Dy() + page.Bounds().Dy() + bottom.Bounds().Dy()
-
-	img := image.NewRGBA(image.Rect(0, 0, outerWidth, outerHeight))
-
-	// pink fill shows any gaps
-//	pink := color.RGBA{255, 0, 255, 255}
-//	draw.Draw(img, img.Bounds(), &image.Uniform{pink}, image.ZP, draw.Src)
-
-	draw.Draw(img, page.Bounds().Add(image.Pt(left.Bounds().Dx(), top.Bounds().Dy())), page, image.ZP, draw.Src)
-
-	// top-left
-	offset := image.ZP
-	draw.Draw(img, topLeft.Bounds().Add(offset), topLeft, image.ZP, draw.Src)
-
-	// top
-	for offset := topLeft.Bounds().Dx(); offset < outerWidth - topRight.Bounds().Dx(); offset += top.Bounds().Dx() {
-		r := top.Bounds().Add(image.Pt(offset, 0))
-		draw.Draw(img, r, top, image.ZP, draw.Src)
-	}
-
-	// top-right
-	offset = image.Pt(outerWidth - topRight.Bounds().Dx(), 0)
-	draw.Draw(img, topRight.Bounds().Add(offset), topRight, image.ZP, draw.Src)
-
-	// left
-	for offset := topLeft.Bounds().Dy(); offset < outerHeight - bottomLeft.Bounds().Dy(); offset += left.Bounds().Dy() {
-		r := left.Bounds().Add(image.Pt(0, offset))
-		draw.Draw(img, r, left, image.ZP, draw.Src)
-	}
-
-	// right
-	for offset := topRight.Bounds().Dy(); offset < outerHeight - bottomRight.Bounds().Dy(); offset += right.Bounds().Dy() {
-		r := right.Bounds().Add(image.Pt(outerWidth - right.Bounds().Dx(), offset))
-		draw.Draw(img, r, right, image.ZP, draw.Src)
-	}
-
-	// bottom-left
-	offset = image.Pt(0, outerHeight - bottomLeft.Bounds().Dy())
-	draw.Draw(img, bottomLeft.Bounds().Add(offset), bottomLeft, image.ZP, draw.Src)
-
-	// bottom
-	for offset := bottomLeft.Bounds().Dx(); offset < outerWidth - bottomRight.Bounds().Dx(); offset += bottom.Bounds().Dx() {
-		r := bottom.Bounds().Add(image.Pt(offset, outerHeight - bottom.Bounds().Dy()))
-		draw.Draw(img, r, bottom, image.ZP, draw.Src)
-	}
-
-	// bottom-right
-	offset = image.Pt(outerWidth - bottomRight.Bounds().Dx(), outerHeight - bottomRight.Bounds().Dy())
-	draw.Draw(img, bottomRight.Bounds().Add(offset), topRight, image.ZP, draw.Src)
-
+	img := theme.Decorate(page)
 
 	writeImage("output.png", img)
 }
